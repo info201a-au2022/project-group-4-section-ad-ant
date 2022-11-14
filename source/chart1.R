b8746686c19bd2930d51a4a6da96333a5c3d5a9e
Thomas Emnetu <thomas@Thomass-MBP-5.home>
2022-11-14 12:26:30 -0800

    renamed data_access.R --> chart1.R

--- source/chart1.R
+++ source/chart1.R
@@ -15,10 +15,7 @@ data_access_test <- function (name="World!") {
 # load .csv into variable 
 wa_cities <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-4-section-ad-ant/main/data/sorted-WA-cities.csv")
 
-#plot chart
-#hist(wa_cities$safetyScore)
-#plot(x=wa_cities$state, y=wa_cities$safetyScore)
-
+# function to plot the relation of Safety Score's to cities (scatterplot)
 plot_wa <- function (df = wa_cities) {
   ggplot(df, aes(x = safetyScore, y = fct_reorder(state, safetyScore, .fun = mean))) +
     geom_point() + geom_vline(xintercept=0, color = "red", linewidth=1) + theme_minimal() + 
