7a2a8bdd63bd851dfe72ef30071689394fa4cb4d
nathannarayana <ibnn@uw.edu>
2022-11-14 21:31:11 -0800

    added chart 3

---
+++ source/chart3.R
@@ -0,0 +1,21 @@
+library("ggplot2")
+library("tidyverse")
+library("dplyr")
+
+
+data <- read.csv("county-crime-WA.csv")
+
+filtered <- data %>%
+  arrange(TOTAL_CRIMES) %>%
+  summarize(County, TOTAL_CRIMES)
+
+# PLOT
+plot1 <- ggplot(filtered, aes(x = TOTAL_CRIMES, y = reorder(County, -TOTAL_CRIMES))) +
+    geom_bar(stat="identity", fill="#f68060", alpha=.6, width=.6) +
+    xlab("Number of Committed Crimes") +
+    theme_bw() +
+    labs(x = "Number of Crimes",
+         y = "County",
+         title = "Total Crimes in Each County in Washington State for the Year 2019")
+
+plot1
