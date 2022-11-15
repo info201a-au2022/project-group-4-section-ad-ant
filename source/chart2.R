63d6e491bf12357ac7b36d7d317cbd08df61039d
Thomas Emnetu <emnettho@uw.edu>
2022-11-14 21:44:57 -0800

    updating alyssas file

--- source/chart2.R
+++ source/chart2.R
@@ -6,8 +6,29 @@ library(stringr)
 library(ggplot2)
 
 # SOURCE
-county_crime <- read.csv("/Users/alyssalmt28/Documents/info201/project-group-4-section-ad-ant/data/NIBRS1220.csv")
+county_crime <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-4-section-ad-ant/main/data/NIBRS1220.csv")
 View(county_crime)
+
+# LIBRARY
+library(ggplot2)
+library(dplyr)
+
+# plot
+plot_v1 <- function (df=county_crime_filtered) {ggplot(county_crime_filtered, 
+                                                       aes(x = INDEXYEAR, 
+                                                           y = PRISON.RATE, 
+                                                           fill = COUNTY)) +
+    geom_area() + 
+    facet_wrap(~COUNTY) +
+    labs(
+      x = "Year",
+      y = "Prison Rate",
+      title = "Prison Rate in Washington Counties from 2012-2020",
+    )
+}
+
+
+plot_v1()
 
 # DATA WRANGLING
 
@@ -20,110 +41,22 @@ county_crime_filtered <- county_crime %>%
             PRISON.RATE,
   )
 
-county_crime_filtered_2012 <- county_crime %>%
-  filter(LOCATION == "COUNTY TOTAL") %>%
-  filter(INDEXYEAR == 2012) %>%
-  group_by(COUNTY) %>%
-  summarize(COUNTY,
-            INDEXYEAR,
-            PRISON.TOTAL,
-            PRISON.RATE,
-            )
 
-county_crime_filtered_2013 <- county_crime %>%
-    filter(LOCATION == "COUNTY TOTAL") %>%
-    filter(INDEXYEAR == 2013) %>%
-    group_by(COUNTY) %>%
-    summarize(COUNTY,
-              INDEXYEAR,
-              PRISON.TOTAL,
-              PRISON.RATE,
-    ) 
-  
-county_crime_filtered_2014 <- county_crime %>%
-  filter(LOCATION == "COUNTY TOTAL") %>%
-  filter(INDEXYEAR == 2014) %>%
-  group_by(COUNTY) %>%
-  summarize(COUNTY,
-            INDEXYEAR,
-            PRISON.TOTAL,
-            PRISON.RATE,
-  ) 
+earliest_record <- county_crime_filtered[1, 2]
 
-county_crime_filtered_2015 <- county_crime %>%
-  filter(LOCATION == "COUNTY TOTAL") %>%
-  filter(INDEXYEAR == 2015) %>%
-  group_by(COUNTY) %>%
-  summarize(COUNTY,
-            INDEXYEAR,
-            PRISON.TOTAL,
-            PRISON.RATE,
-  ) 
+latest_record <- county_crime_filtered[8, 2]
 
-county_crime_filtered_2016 <- county_crime %>%
-  filter(LOCATION == "COUNTY TOTAL") %>%
-  filter(INDEXYEAR == 2016) %>%
-  group_by(COUNTY) %>%
-  summarize(COUNTY,
-            INDEXYEAR,
-            PRISON.TOTAL,
-            PRISON.RATE,
-  ) 
+number_of_counties <- county_crime_filtered %>%
+  summarize(COUNTY) %>%
+  length(unique(COUNTY))
 
-county_crime_filtered_2017 <- county_crime %>%
-  filter(LOCATION == "COUNTY TOTAL") %>%
-  filter(INDEXYEAR == 2017) %>%
-  group_by(COUNTY) %>%
-  summarize(COUNTY,
-            INDEXYEAR,
-            PRISON.TOTAL,
-            PRISON.RATE,
-  ) 
+lowest_prison_rate <- county_crime_filtered %>%
+  filter(PRISON.RATE == 0) %>%
+  pull(COUNTY)
 
-county_crime_filtered_2018 <- county_crime %>%
-  filter(LOCATION == "COUNTY TOTAL") %>%
-  filter(INDEXYEAR == 2018) %>%
-  group_by(COUNTY) %>%
-  summarize(COUNTY,
-            INDEXYEAR,
-            PRISON.TOTAL,
-            PRISON.RATE,
-  ) 
+highest_prison_rate <- county_crime_filtered %>%
+  filter(PRISON.RATE > 35) %>%
+  pull(COUNTY)
 
-county_crime_filtered_2019 <- county_crime %>%
-  filter(LOCATION == "COUNTY TOTAL") %>%
-  filter(INDEXYEAR == 2019) %>%
-  group_by(COUNTY) %>%
-  summarize(COUNTY,
-            INDEXYEAR,
-            PRISON.TOTAL,
-            PRISON.RATE,
-  ) 
 
-county_crime_filtered_2020 <- county_crime %>%
-  filter(LOCATION == "COUNTY TOTAL") %>%
-  filter(INDEXYEAR == 2020) %>%
-  group_by(COUNTY) %>%
-  summarize(COUNTY,
-            INDEXYEAR,
-            PRISON.TOTAL,
-            PRISON.RATE,
-  ) 
 
-country_crime_filtered_average <- country_crime %>%
-  filter(LOCATION == "COUNTY TOTAL") %>%
-  
-
-# LIBRARY
-library(ggplot2)
-library(dplyr)
-
-# plot
-plot_v1 <- ggplot(county_crime_filtered, 
-                  aes(x = INDEXYEAR, 
-                      y = PRISON.RATE, 
-                      fill = COUNTY)) +
-  geom_area() + 
-  facet_wrap(~COUNTY)
-  facet_wrap
-  
